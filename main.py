6fb9cd73924ba83da33973e49369138980e4965d
QuentinAbn <quentin.aubineau@epfedu.fr>
2023-09-19 13:23:27 +0200

    new try to make ganalytics work

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-285832196-2"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-MJH545RKJV"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', 'UA-285832196-2');
+ gtag('config', 'G-MJH545RKJV');
 </script>
  """
  return prefix_google + "Hello World"
