d2a997e70cdcd1d78773ab0103f313b9240e49a2
QuentinAbn <quentin.aubineau@epfedu.fr>
2023-09-19 12:47:54 +0200

    Update of the app

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-    return "Hello from Space! 🚀"
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-285832196-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-285832196-1');
+</script>
+ """
+ return prefix_google + "Hello World"
